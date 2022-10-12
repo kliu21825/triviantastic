e0537a1837821c3821ca7b3d3474355830c0f045
Kevin Liu <kliu@southtexascollege.edu>
2022-10-12 16:51:23 -0500

    fix logo import

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -33,8 +33,10 @@ onMounted(async () => {
 <style lang="postcss" scoped>
 .categories {
   @apply grid flex-grow grid-cols-4 gap-12;
+
   & .category {
     @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+
     &:hover {
       @apply cursor-pointer border-red-500 bg-red-500 text-white;
     }
